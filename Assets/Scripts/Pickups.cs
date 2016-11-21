baa95b29272ad85c749ed359376641833d3d1094
jthra037 <its_thrasher@live.ca>
2016-11-21 14:07:56 -0500

    Add TempPickup object to Jacob's scene Add Pickups.cs

---
+++ Assets/Scripts/Pickups.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pickups : MonoBehaviour {
+
+}
+
+public class SpeedPickup : Pickups {
+
+}
+
+public class StrengthPickup : Pickups {
+
+}
