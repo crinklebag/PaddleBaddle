8b5dd10f1633f2b62575388b41e49ddbcedce493
jthra037 <its_thrasher@live.ca>
2016-11-24 13:20:25 -0500

    Add the ability to pick up the pickups Change setup of JacobScene

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -7,9 +7,19 @@ public class Pickup : MonoBehaviour {
 	void OnTriggerEnter(Collider other)
 	{
 		Debug.Log ("Do the pickup thing");
-		if (other.CompareTag ("Player")) {
+		if (other.CompareTag ("Player"))
+		{
 			other.GetComponent<Boat> ().hasPowerUp = true;
 			other.GetComponent<Boat> ().powerUpType = type;
+
+			gameObject.GetComponent<Collider> ().isTrigger = false;
+			transform.parent = other.transform;
+
+			transform.localPosition = new Vector3 (0, 0, 0.5f);
+			transform.localRotation = Quaternion.identity;
+
+			Destroy (gameObject.GetComponent<RealisticBuoyancy> ());
+			Destroy(gameObject.GetComponent<Rigidbody>());
 		}
 	}
 }
