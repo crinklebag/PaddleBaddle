a7d589b02ce01058905583dc46cf3a922f8c955a
Jacob <its_thrasher@live.ca>
2017-01-17 12:47:37 -0500

    Add BirdSpawner script Add in-editor gizmo to represent  flight path to endPoint Add prefab of BirdSpawner object

---
+++ Assets/Scripts/BirdSpawner.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BirdSpawner : MonoBehaviour {
+    [HideInInspector]
+    public Vector3 startPos;
+    [HideInInspector]
+    public Vector3 endPos;
+
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private float minTime;
+    [SerializeField]
+    private float maxTime;
+    [SerializeField]
+    private float waitTime;
+
+	// Use this for initialization
+	void Start () {
+        startPos = transform.position;
+        endPos = target.position;
+	}
+
+    void OnDrawGizmosSelected()
+    {
+        if (target != null)
+        {
+            // Draws a blue line from this transform to the target
+            Gizmos.color = Color.red;
+            Gizmos.DrawLine(transform.position, target.position);
+        }
+    }
+}
