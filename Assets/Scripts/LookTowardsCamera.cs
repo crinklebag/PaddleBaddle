3a4ee03cab295d5c54b997523dec32e8f642af42
Rebecca <rebeccamitchell88@gmail.com>
2017-02-10 18:34:36 -0500

    Added in a GD raft

--- Assets/Scripts/LookTowardsCamera.cs
+++ Assets/Scripts/LookTowardsCamera.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class LookTowardsCamera : MonoBehaviour
 {
     public Camera m_Camera;
-    public bool isActive = false;
+    // public bool isActive = false;
     public bool autoInit = false;
     GameObject myContainer;
 
@@ -13,22 +13,22 @@ public class LookTowardsCamera : MonoBehaviour
         if (autoInit == true)
         {
             m_Camera = Camera.main;
-            isActive = true;
+            // isActive = true;
         }
 
-        myContainer = new GameObject();
+        /*myContainer = new GameObject();
         myContainer.name = "GRP_" + transform.gameObject.name;
         myContainer.transform.position = transform.position;
         myContainer.transform.parent = transform.parent;
-        transform.parent = myContainer.transform;
+        transform.parent = myContainer.transform;*/
     }
 
-
-    void Update()
-    {
-        if (isActive == true)
+    void Update() {
+        Debug.Log("Looking at comera");
+        if (this.gameObject.activeSelf)
         {
-            myContainer.transform.LookAt(myContainer.transform.position + m_Camera.transform.rotation * Vector3.back, m_Camera.transform.rotation * Vector3.up);
+            Debug.Log("Looking at comera");
+            transform.LookAt(transform.position + m_Camera.transform.rotation * Vector3.back, m_Camera.transform.rotation * Vector3.up);
         }
     }
 }
