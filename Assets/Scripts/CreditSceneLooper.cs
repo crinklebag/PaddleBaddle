bc8928dbfabc1450938f924a531fef77d2496e18
IAN! <Iannorton01@gmail.com>
2017-03-01 19:51:04 -0500

    Push race scene updates into develop. added in girl standing animations and cut them up.

---
+++ Assets/Scripts/CreditSceneLooper.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Rewired;
+using UnityEngine.SceneManagement;
+
+public class CreditSceneLooper : MonoBehaviour {
+    [SerializeField] int playerID;
+
+    Player player;
+
+    // Use this for initialization
+    void Awake () {
+        player = ReInput.players.GetPlayer(playerID);
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (player.GetButtonDown("Attack"))
+        {
+            Debug.Log("FOOP");
+            SceneManager.LoadScene("Lobby Design");
+        }
+	}
+}
