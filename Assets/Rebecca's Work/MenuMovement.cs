540e64f650521ffd3b634501bf60034159002d61
Rebecca Mitchekk <rebeccamitchell88@gmail.com>
2017-03-06 14:55:09 -0500

    Lots of scene changes Cleaned up the materials - what is popping off with those things Set up the boat selection and level select cleaned up the main levels - ians swamp and the fighting arena

---
+++ Assets/Rebecca's Work/MenuMovement.cs
@@ -0,0 +1,148 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Rewired;
+
+public class MenuMovement : MonoBehaviour {
+
+    public enum BoatType { CANOE, RAFT };
+    [SerializeField] BoatType currentBoat; 
+
+    [SerializeField] SecondMenuController menuController;
+
+    [Header("Boat Selection Variables")]
+    [SerializeField] Image[] boatUI;
+    [SerializeField] GameObject[] boatBody;
+    [SerializeField] GameObject boatContents;
+       
+    [Header("Movement Variables")]
+    [SerializeField] GameObject paddlePivot;
+    [SerializeField] GameObject paddle;
+    [SerializeField] int playerID;
+    [SerializeField] float paddleForwardForce = 500;
+    [SerializeField] float paddleTorque = 250;
+    [SerializeField] float paddleRoationSpeed;
+
+    Player player;
+    GameObject boat;
+    Quaternion initRot;
+    int dir = 0;
+    float paddleRotationTimer = 0;
+    bool canPaddle = true;
+    bool canInput = true;
+    bool inGame = true;
+
+    // Use this for initialization
+    void Awake () {
+        if (playerID == 1 || playerID == 3) {
+            Debug.Log("Checking");
+            inGame = menuController.IsFourPlayer();
+        }
+        player = ReInput.players.GetPlayer(playerID);
+        boat = this.gameObject;
+        currentBoat = BoatType.CANOE;
+        if(paddle != null) initRot = paddle.transform.localRotation;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (menuController.CanMove())
+        {
+            if (player.GetButtonDown("+Right Paddle") && canPaddle)
+            {
+                paddlePivot.transform.localScale = new Vector3(1, 1, 1);
+                //Go Forward
+                dir = 1;
+                MoveCanoe();
+            }
+            else if (player.GetButtonDown("-Right Paddle") && canPaddle)
+            {
+                paddlePivot.transform.localScale = new Vector3(1, 1, 1);
+                // Go Backward
+                dir = -1;
+                MoveCanoe();
+
+            }
+            else if (player.GetButtonDown("+Left Paddle") && canPaddle)
+            {
+                paddlePivot.transform.localScale = new Vector3(-1, 1, 1);
+                //Go Forward
+                dir = 1;
+                MoveCanoe();
+            }
+            else if (player.GetButtonDown("-Left Paddle") && canPaddle)
+            {
+                paddlePivot.transform.localScale = new Vector3(-1, 1, 1);
+                // Go Backward
+                dir = -1;
+                MoveCanoe();
+            }
+        }
+        else {
+            // Swap Boats
+            if ((player.GetAxis("Horizontal 1") > 0.34f || player.GetAxis("Horizontal 1") < -0.34f) && canInput) {
+                // Get Next Boat
+                if (currentBoat < BoatType.RAFT) {
+                    currentBoat = BoatType.RAFT;
+                    boatUI[(int)currentBoat].gameObject.SetActive(true);
+                    boatUI[(int)currentBoat - 1].gameObject.SetActive(false);
+                    boatBody[(int)currentBoat].SetActive(true);
+                    boatBody[(int)currentBoat - 1].SetActive(false);
+                    boatContents.transform.parent = boatBody[(int)currentBoat].transform;
+                } else {
+                    currentBoat = BoatType.CANOE;
+                    boatUI[(int)currentBoat].gameObject.SetActive(true);
+                    boatUI[(int)currentBoat + 1].gameObject.SetActive(false);
+                    boatBody[(int)currentBoat].SetActive(true);
+                    boatBody[(int)currentBoat + 1].SetActive(false);
+                    boatContents.transform.parent = boatBody[(int)currentBoat].transform;
+                }
+
+                StartCoroutine(WaitForInput());
+            }
+        }
+
+        RotatePaddle();
+    }
+
+    void MoveCanoe()
+    {
+        // Add force to boat by the paddle
+        Debug.Log("Adding Forward Force");
+        canPaddle = false;
+
+        Vector3 finalForwardForce = dir * paddleForwardForce * boat.transform.forward;
+        boat.transform.GetComponentInChildren<Rigidbody>().AddForceAtPosition(finalForwardForce, boat.transform.position, ForceMode.Impulse);
+
+        float horizontalDirection = Mathf.Sign(dir * paddle.transform.localPosition.x * paddlePivot.transform.localScale.x);
+        Vector3 finalHorizontalForce = horizontalDirection * paddleTorque * boat.transform.up;
+        boat.transform.GetComponentInChildren<Rigidbody>().AddTorque(finalHorizontalForce, ForceMode.Impulse);
+
+    }
+
+    void RotatePaddle()
+    {
+        if (!canPaddle)
+        {
+            paddle.transform.RotateAround(paddlePivot.transform.position, paddlePivot.transform.right * (dir * -1), paddleRoationSpeed * Time.deltaTime);
+            paddle.transform.localRotation = initRot;
+            paddleRotationTimer += paddleRoationSpeed * Time.deltaTime;
+            if (paddleRotationTimer >= 360)
+            {
+                canPaddle = true;
+                paddleRotationTimer = 0;
+            }
+        }
+    }
+
+    IEnumerator WaitForInput() {
+
+        canInput = false;
+
+        yield return new WaitForSeconds(0.3f);
+
+        canInput = true;
+
+    }
+}
---
+++ Assets/Rebecca's Work/SecondMenuController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SecondMenuController : MonoBehaviour {
+
+    [SerializeField] GameObject Team1Player2;
+    [SerializeField] GameObject Team1Paddle;
+    [SerializeField] GameObject Team2Player4;
+    [SerializeField] GameObject Team2Paddle;
+
+    bool canMove = false;
+    bool fourPlayers = false;
+
+    bool teamOneSelected = false;
+    bool teamTwoSelected = false;
+
+	// Use this for initialization
+	void Start () {
+        // Check if 2 or 4 player game
+        if (PlayerPrefs.GetInt("numPlayers") == 4) { fourPlayers = true; }
+        else {
+            fourPlayers = false;
+            SetUpTwoPlayers();
+        }
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (teamOneSelected && teamTwoSelected) {
+            canMove = true;
+        }
+	}
+
+    void SetUpTwoPlayers() {
+        Team1Player2.SetActive(false);
+        Team1Paddle.SetActive(false);
+        Team2Paddle.SetActive(false);
+        Team2Player4.SetActive(false);
+    }
+
+    public void TeamOneSelect(string boatSelection) {
+        teamOneSelected = true;
+        PlayerPrefs.SetString("teamOneBoat", boatSelection);
+    }
+
+    public void TeamOneDeselect() {
+        teamOneSelected = false;
+        PlayerPrefs.SetString("teamOneBoat", "");
+    }
+
+    public void TeamTwoSelect(string boatSelection) {
+        teamTwoSelected = true;
+        PlayerPrefs.SetString("teamOneBoat", boatSelection);
+    }
+
+    public void TeamTwoDeselect() {
+        teamTwoSelected = false;
+        PlayerPrefs.SetString("teamOneBoat", "");
+    }
+
+    public bool CanMove() {
+        return canMove;
+    }
+
+    public bool IsFourPlayer() {
+        return fourPlayers;
+    }
+}
