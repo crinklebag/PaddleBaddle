4ca3a45c3764a5ec551c996b58229fde506e6cf5
IAN AT HOME <Iannorton01@gmail.com>
2017-03-19 00:31:22 -0400

    floating +1 txt when you get a pick up.

--- Assets/Scripts/PickupParticles.cs
+++ Assets/Scripts/PickupParticles.cs
@@ -4,13 +4,16 @@ using UnityEngine;
 
 public class PickupParticles : MonoBehaviour {
     [SerializeField] GameObject particles;
+    [SerializeField] GameObject plusOneText;
 
    void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             GameObject newParticles = Instantiate(particles, transform.position, Quaternion.identity) as GameObject;
+            GameObject newPlusOneText = Instantiate(plusOneText, transform.position, Quaternion.Euler(0,-90,0)) as GameObject;
             Destroy(newParticles, 3);
+            Destroy(newPlusOneText, 1);
         }
     }
 }
