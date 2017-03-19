255a424c8966270ed2d586597bc9aab6865ec942
IAN! <Iannorton01@gmail.com>
2017-03-18 21:50:18 -0400

    Put a bottom on thechest, made particle effect, Gold Material. script for chest pickup effects.

---
+++ Assets/Scripts/PickupParticles.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickupParticles : MonoBehaviour {
+    [SerializeField] GameObject particles;
+
+   void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            GameObject newParticles = Instantiate(particles, transform.position, Quaternion.identity) as GameObject;
+            Destroy(newParticles, 3);
+        }
+    }
+}
